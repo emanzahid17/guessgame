2ab0214bf165b1096a5b698d7c35aa8b0adee910
emanzahid17 <ezahid834@gmail.com>
2024-09-04 20:30:29 +0500

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,13 @@
+import streamlit
+import random
+from random import randint
+
+guess_num = random.randint(1,100)
+while True:
+    entered_num = int(input('enter number: '))
+    if entered_num > guess_num:
+        print("Too high Guess, Try again!")
+    elif entered_num < guess_num:
+        print("Too low Guess, Try again!")
+    else:
+        print("Congratulations! You guessed the Correct Number")
